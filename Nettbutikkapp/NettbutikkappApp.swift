b9e438b8e0beca7515044a9953849d1fe654179b
Nicolai Andreassen <94894772+Niana001@users.noreply.github.com>
2023-03-13 14:24:45 +0100

    Initial Commit

---
+++ Nettbutikkapp/NettbutikkappApp.swift
@@ -0,0 +1,17 @@
+//
+//  NettbutikkappApp.swift
+//  Nettbutikkapp
+//
+//  Created by Nicolai Bacus Andreassen on 13/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct NettbutikkappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
